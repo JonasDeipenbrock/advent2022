b32900c7517a48d9ade35a5999882dddfe5712ea
Jonas Deipenbrock <jonas.deipenbrock@gmx.de>
2022-12-01 23:04:20 +0100

    adds base file for python
    Signed-off-by: Jonas Deipenbrock <jonas.deipenbrock@gmx.de>

---
+++ base.py
@@ -0,0 +1,17 @@
+def get_input():
+    with open("./input.txt", "r") as f:
+        return f.readlines()
+
+
+def solve_a(i):
+    pass
+
+
+def solve_b(i):
+    pass
+
+
+if __name__ == "__main__":
+    i = get_input()
+    solve_a(i)
+    solve_b(i)
