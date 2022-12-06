36778aa54a3d8b9e5f41fb7545f6aa828cc573d1
Jonas Deipenbrock <jonas.deipenbrock@gmx.de>
2022-12-06 23:10:40 +0100

    shorter version
    Signed-off-by: Jonas Deipenbrock <jonas.deipenbrock@gmx.de>

--- six/short.py
+++ six/short.py
@@ -2,8 +2,5 @@
 with open("./input.txt", "r") as f:
     line = f.readline()
     def find(length):
-        substr = [line[i : i + length] for i in range(len(line))]
-        return [idx + length for idx, hit in enumerate(substr) if isUnique(hit)][0]
-    def isUnique(str):
-        return len(list(set(str))) == len(list(str))
-    print(find(4), find(14))
+        print([idx + length for idx, hit in enumerate([line[i : i + length] for i in range(len(line))]) if len(list(set(hit))) == len(list(hit))][0])
+    find(4), find(14)
