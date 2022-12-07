9f5361125d3f754747f66c981a3181e80dc4917c
Jonas Deipenbrock <jonas.deipenbrock@gmx.de>
2022-12-07 13:35:09 +0100

    solves day seven with dict
    Signed-off-by: Jonas Deipenbrock <jonas.deipenbrock@gmx.de>

--- seven/main.py
+++ seven/main.py
@@ -1,97 +1,55 @@
 from typing import List
-
-File = (str, int)
-Dir = (str, int, [File])
-
-
-class File:
-    name: str
-    size: int
-
-    def __init__(self, name, size) -> None:
-        self.name = name
-        self.size = size
-
-    def __str__(self) -> str:
-        return self.name
-
-
-class Dir:
-    name: str
-    size: int
-    parent: Dir
-    files = []
-    dirs = []
-
-    def __init__(self, name, parent) -> None:
-        self.name = name
-        self.parent = parent
-
-    def add_file(self, file):
-        self.files.append(file)
-
-    def add_dir(self, dir):
-        self.dirs.append(dir)
-
-    def find_dir(self, name):
-        print(f"looking for {name} in {self.name}")
-        if self.name == name:
-            return self
-        for dir in self.dirs:
-            if dir.find_dir(name):
-                return dir
-        return None
-
-    def get_size(self):
-        for dir in self.dirs:
-            self.size += dir.get_size()
-        for file in self.files:
-            self.size += file.size
-        return self.size
-
-    # def __str__(self) -> str:
-    #     string_arr = [self.name]
-    #     for file in self.files:
-    #         string_arr.append(file.__str__())
-    #     for dir in self.dirs:
-    #         string_arr.append(dir.__str__())
-    #     return self.name
+from collections import defaultdict
 
 
 def get_input():
-    with open("./seven/example.txt", "r") as f:
+    with open("./input.txt", "r") as f:
         return f.readlines()
 
 
-def solve_a(input):
-    root = read_input(input)
+def solve_a(input: List[str]):
+    dir = defaultdict(int)
+    root = []
+    for line in input:
+        match line.split():
+            # current dir done, move to next in line
+            case ["$", "cd", ".."]:
+                root.pop()
+            # add dir to processing
+            case ["$", "cd", d]:
+                root.append(d)
+            # ignore ls commands
+            case ["$", "ls"]:
+                pass
+            # ignore reads from ls => get handles via `cd a`
+            case ["dir", d]:
+                pass
+            # sum files
+            case [size, file]:
+                # add file size to index
+                dir[tuple(root)] += int(size)
+                # take all parent paths
+                path = root[:-1]
+                # for each parent path add file size to their size
+                while path:
+                    dir[tuple(path)] += int(size)
+                    path.pop()
+
+    # filter all dirs with value <= 100000 and sum them
+    small_dirs = [d for d in dir.values() if d <= 100000]
+    sum_dirs = sum(small_dirs)
+    print(sum_dirs)
+
+    # calculate needed space
+    available_space = 70000000 - dir[("/",)]
+    needed_space = 30000000
+    possible_dirs = [d for d in dir.values() if d + available_space >= needed_space]
+    best_dir = min(possible_dirs)
+    print(best_dir)
 
 
 def solve_b(input):
     pass
-
-
-def read_input(input: List[str]) -> Dir:
-    root_dir = Dir("/", None)
-    curr_dir = root_dir
-    for line in input:
-        front, back, *args = line.strip().split()
-        if front != "$":
-            if front == "dir":
-                dir = Dir(back, curr_dir)
-                print(back, curr_dir.name)
-                curr_dir.add_dir(dir)
-            else:
-                file = File(back, front)
-                print(back, curr_dir.name)
-                curr_dir.add_file(file)
-        elif front == "$":
-            if back == "cd":
-                if args[0] == "..":
-                    curr_dir = curr_dir.parent
-                else:
-                    curr_dir = root_dir.find_dir(args[0])
-                print("switching to", curr_dir.name)
 
 
 if __name__ == "__main__":
